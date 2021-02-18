3ae138e8bb1b2b89abdb9f9d5de1b95b8f549d1b
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-02-18 17:15:34 +0100

    loader json og generer tilfældige i single view

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let art;
 
-const url = "";
-const media = "";
+const url = "https://vnduprojekt-ec80.restdb.io/rest/vndu";
+const media = "https://vnduprojekt-ec80.restdb.io/media/";
 const options = {
     headers: {
-        'x-apikey': ""
+        'x-apikey': "602e74535ad3610fb5bb6333"
     }
 };
 
@@ -20,5 +20,21 @@ async function loadJSON() {
 }
 
 function showArt() {
+    console.log("showingArt");
     console.log(art);
+    const template = document.querySelector("template");
+    const container = document.querySelector(".container")
+
+    art.forEach(artwork => {
+        let clone = template.cloneNode(true).content;
+        clone.querySelector("img").src = media + artwork.billede[0];
+        clone.querySelector("img").alt = artwork.kort;
+        clone.querySelector("article").addEventListener("click", () => showDetails(artwork));
+        container.appendChild(clone);
+    })
+}
+
+function showDetails(artwork) {
+    console.log("showDetails");
+    location.href = `artwork.html?id=${artwork._id}`;
 }
