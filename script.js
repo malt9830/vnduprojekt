9abbf9b48a3ae44a9f7eaebf3d785e715fed2e3a
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-02-19 15:12:05 +0100

    gallery partial

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let art;
 
+//Definerer konstanter til senere brug for fetch af json
 const url = "https://vnduprojekt-ec80.restdb.io/rest/vndu";
 const media = "https://vnduprojekt-ec80.restdb.io/media/";
 const options = {
@@ -14,6 +15,7 @@ const options = {
 };
 
 async function loadJSON() {
+    //Henter json og gemmer det som art
     const JSONData = await fetch(url, options);
     art = await JSONData.json();
     showArt();
@@ -22,6 +24,8 @@ async function loadJSON() {
 function showArt() {
     console.log("showingArt");
     console.log(art);
+
+    //Genererer et nyt array af tilfældige objekter fra det komplette array
     const other1 = art[Math.floor(Math.random() * art.length)]
     const other2 = art[Math.floor(Math.random() * art.length)]
     const other3 = art[Math.floor(Math.random() * art.length)]
@@ -29,8 +33,11 @@ function showArt() {
     console.log(randomArt);
 
     randomArt.forEach(artwork => {
+        //Definerer konstanter til senere brug i kloningen af template
         const template = document.querySelector("template");
         const container = document.querySelector(".container")
+
+        //Kloner template og udfylder det med data fra de tilfældige objekter
         let clone = template.cloneNode(true).content;
         clone.querySelector("img").src = media + artwork.billede[0];
         clone.querySelector("img").alt = artwork.kort;
