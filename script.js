959812f13c361d199d06ba611b8b59670c387589
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-02-19 12:06:26 +0100

    sider tilføjet + foreløbig forside

--- script.js
+++ script.js
@@ -22,13 +22,21 @@ async function loadJSON() {
 function showArt() {
     console.log("showingArt");
     console.log(art);
-    const template = document.querySelector("template");
-    const container = document.querySelector(".container")
+    const other1 = art[Math.floor(Math.random() * art.length)]
+    const other2 = art[Math.floor(Math.random() * art.length)]
+    const other3 = art[Math.floor(Math.random() * art.length)]
+    const randomArt = [other1, other2, other3];
+    console.log(randomArt);
 
-    art.forEach(artwork => {
+    randomArt.forEach(artwork => {
+        const template = document.querySelector("template");
+        const container = document.querySelector(".container")
         let clone = template.cloneNode(true).content;
         clone.querySelector("img").src = media + artwork.billede[0];
         clone.querySelector("img").alt = artwork.kort;
+        clone.querySelector("h2").textContent = artwork.navn;
+        clone.querySelector("h3").textContent = `Af ${artwork.kunstner}`;
+        clone.querySelector("p").textContent = artwork.kort;
         clone.querySelector("article").addEventListener("click", () => showDetails(artwork));
         container.appendChild(clone);
     })
