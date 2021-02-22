7187e2b9b2c941af7219f9a2092b97a7ebabd532
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-02-22 02:26:05 +0100

    struktureret mapper

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,16 +1,15 @@
-         //Med et klik på menuknappen kaldes funktionen toggleMenu
-         document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+//Med et klik på menuknappen kaldes funktionen toggleMenu
+document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
-         function toggleMenu() {
-             console.log("toggleMenu");
-             document.querySelector("#oversigt").classList.toggle("hidden");
-             document.querySelector("#oversigt").classList.toggle("slide");
-             myFunction(this);
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#oversigt").classList.toggle("hidden");
+    document.querySelector("#oversigt").classList.toggle("slide");
+    myFunction(this);
 
-             // this henvender sig til menuknappen. Så når man killer på menuknappen, kalder den både togglemenu og myFuncking. Har skrvet nyfincken (this) her for at den så også går med og kalder funktionen nedenunder
-         }
+    // this henvender sig til menuknappen. Så når man killer på menuknappen, kalder den både togglemenu og myFuncking. Har skrvet nyfincken (this) her for at den så også går med og kalder funktionen nedenunder
+}
 
-
-         function myFunction(x) {
-             x.classList.toggle("change");
-         }
+function myFunction(x) {
+    x.classList.toggle("change");
+}
