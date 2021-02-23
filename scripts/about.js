1986b2b4625d3a69f43b9931ea38b70eb7714bc9
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-02-23 11:49:10 +0100

    tilføjet manglende js-kommentarer på about-siden

--- scripts/about.js
+++ scripts/about.js
@@ -3,14 +3,17 @@ const artistButtons = document.querySelectorAll(".button_artist");
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded");
 
+    //Tilføjer knap-funktionen til de 4 kunsterknapper
     artistButtons.forEach(button => button.addEventListener("click", loadArtistPage))
 });
 
 function loadArtistPage() {
     console.log("loadArtistPage");
 
+    //Henter navnet fra søskendeelementets h1 til både url og filter på den næste loadede side
     const artistName = this.parentElement.firstElementChild.firstElementChild.textContent.split(" ").join("_");
     console.log(artistName);
 
+    //Anvender ovenstående konstant som id til kunstnersidens url
     location.href = `artist.html?id=${artistName}`;
 }
