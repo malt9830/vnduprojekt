3ae105d51ab80bb2312b6f692774207f829e7f9f
Ida-23 <idax7887@stud.kea.dk>
2021-02-24 11:23:37 +0100

    burgernemu js forklarende tekst tilført

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,6 +1,8 @@
 //Med et klik på menuknappen kaldes funktionen toggleMenu
 document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
+
+// Her i funktioen bliver css classerne hidden samt slide togglet, så menuens indholde bliver synlig.
 function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#oversigt").classList.toggle("hidden");
@@ -10,6 +12,8 @@ function toggleMenu() {
     // this henvender sig til menuknappen. Så når man killer på menuknappen, kalder den både togglemenu og myFuncking. Har skrvet nyfincken (this) her for at den så også går med og kalder funktionen nedenunder
 }
 
+
+// Her i funktionen ændres brugernemuens tre streger til et kryds
 function myFunction(x) {
     x.classList.toggle("change");
 }
