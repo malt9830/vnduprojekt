688b41b05f3d93d43232d4128923bdd1b7655108
Ida-23 <idax7887@stud.kea.dk>
2021-02-24 10:42:39 +0100

    galleri js  har fået mere uddybende kommentarer

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,24 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
     loadJSON();
 })
 
-//Definerer filtervariabel og -funktion
+//Her definerers filtervariabel og -funktion
 let filter = "Alle";
 const filterButtons = document.querySelectorAll("#section_gallery nav button");
-filterButtons.forEach(button => button.addEventListener("click", filterArt));
+filterButtons.forEach(button => button.addEventListener("click", filterArt)); //her lægges en eventlisner på alle filteringsknapper, og sender ved klik en ned i funktionen filterArt.
 
+
+
+// Funktionen der filtrer indholdet på siden, alt efter hvad det klikkes på
 function filterArt() {
-    filter = this.dataset.materiale;
+    filter = this.dataset.materiale; // Her sættes filteret lig med matiralet på objektet
     console.log(filter);
 
-    document.querySelector(".selected").classList.remove("selected");
-    this.classList.add("selected");
+    document.querySelector(".selected").classList.remove("selected"); //fjerner css class med det lyserøde baggrundsfarve
+
+    this.classList.add("selected"); // tilføjer class med den lyserøde baggrundsfarve
 
     showArt();
 }
 
+
 let art;
 
-//Definerer konstanter til senere brug for fetch af json
+//Her defineres konstanter til senere brug for fetch af json
 const url = "https://vnduprojekt-ec80.restdb.io/rest/vndu";
 const media = "https://vnduprojekt-ec80.restdb.io/media/";
 const options = {
@@ -29,6 +34,9 @@ const options = {
     }
 };
 
+
+
+// Her hentes json ind fra restdb, og sendes vider til funktionen showArt
 async function loadJSON() {
     //Henter json og gemmer det som art
     const JSONData = await fetch(url, options);
@@ -36,30 +44,37 @@ async function loadJSON() {
     showArt();
 }
 
+
+// I funktionen showArt, sættes hvert enkelt kunstværk ind i HTML
 function showArt() {
     console.log("showingArt");
     console.log(art);
-    //Definerer konstanter til senere brug i kloningen af template
+
+    //Her definerers konstanter til senere brug i kloningen af template
     const template = document.querySelector("template");
     const container = document.querySelector(".container")
-    container.textContent = "";
+
+    container.textContent = ""; // HTML containeren tømmes for eksisterende indhold, og kan nu få tilført nyt indhold.
 
     art.forEach(artwork => {
         if (filter == "Alle" || filter == artwork.materiale) {
             console.log("looping");
-            //Kloner template og udfylder det med data fra de tilfældige objekter
-            let clone = template.cloneNode(true).content;
+
+            let clone = template.cloneNode(true).content; //Her klones template og udfyldes det med data fra de tilfældige objekter
+
             clone.querySelector("img").src = media + artwork.billede[0];
             clone.querySelector("img").alt = artwork.kort;
             clone.querySelector("h2").textContent = artwork.navn;
             clone.querySelector("h3").textContent = `Af ${artwork.kunstner}`;
             clone.querySelector("p").textContent = artwork.kort;
             clone.querySelector("article").addEventListener("click", () => showDetails(artwork));
-            container.appendChild(clone);
+            container.appendChild(clone); //kolene tilføres til DOM
         }
     })
 }
 
+
+// Når der klikkes på et enkelt kunstværk, føres du ind i et single-view
 function showDetails(artwork) {
     console.log("showDetails");
     location.href = `artwork.html?id=${artwork._id}`;
